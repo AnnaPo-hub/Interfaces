96130f599de68c427cb3756f2c307a8c3f4ff2ef
Anna Popova <anna.popova201909@gmail.com>
2020-04-07 15:09:33 +0300

    corrected variables

--- src/main/java/ru/netology/domain/Manager.java
+++ src/main/java/ru/netology/domain/Manager.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 public class Manager {
     TicketRepo list;
 
-    public Ticket[] findAll(String departureAirport, String arrivalAirport) {
+    public Ticket[] findAll(String from, String to) {
         Ticket[] newList = new Ticket[0];
         for (Ticket ticket : list.getAll()) {
-            if (ticket.arrivalAirport.equals(arrivalAirport) && ticket.departureAirport.equals(departureAirport)) {
+            if (ticket.arrivalAirport.equals(to) && ticket.departureAirport.equals(from)) {
                 Ticket[] tmp = new Ticket[newList.length + 1];
                 System.arraycopy(newList, 0, tmp, 0, newList.length);
                 tmp[tmp.length - 1] = ticket;
